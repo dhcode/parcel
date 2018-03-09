2093d8a267d399b04aa816f5a009d6dc036611ee
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2018-03-10 00:02:31 +0100

    Ignore require if it is defined

--- src/visitors/dependencies.js
+++ src/visitors/dependencies.js
@@ -37,7 +37,8 @@ module.exports = {
       types.isIdentifier(callee) &&
       callee.name === 'require' &&
       args.length === 1 &&
-      types.isStringLiteral(args[0]);
+      types.isStringLiteral(args[0]) &&
+      !hasRequireDefined(ancestors);
 
     if (isRequire) {
       let optional = ancestors.some(a => types.isTryStatement(a)) || undefined;
@@ -85,6 +86,59 @@ module.exports = {
     }
   }
 };
+
+const scopeLookupCache = new WeakMap();
+
+function hasRequireDefined(ancestors) {
+  return (
+    ancestors
+      // reverse to have the current node at [0] and the outermost at [len - 1]
+      .reverse()
+      .some(ancestor => {
+        let defined = scopeLookupCache.get(ancestor);
+
+        if (defined) {
+          return true;
+        }
+
+        defined = hasRequireBinding(ancestor);
+        scopeLookupCache.set(ancestor, defined);
+
+        return defined;
+      })
+  );
+}
+
+function hasRequireBinding(node) {
+  // if it's something with a body
+  if (
+    types.isProgram(node) ||
+    types.isBlockStatement(node) ||
+    types.isBlock(node)
+  ) {
+    return node.body.some(hasRequireBinding);
+  } else if (
+    types.isFunctionDeclaration(node) ||
+    types.isFunctionExpression(node) ||
+    types.isArrowFunctionExpression(node)
+  ) {
+    return (
+      // if it has a name and is named require
+      (node.id && node.id.name === 'require') ||
+      // if it has a parameter named require
+      node.params.some(
+        param => types.isIdentifier(param) && param.name === 'require'
+      )
+    );
+  } else if (types.isVariableDeclaration(node)) {
+    // if it has a variable named require
+    return node.declarations.some(
+      declaration => declaration.id.name === 'require'
+    );
+  }
+
+  return false;
+}
 
 function addDependency(asset, node, opts = {}) {
   if (asset.options.target !== 'browser') {
